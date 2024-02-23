23c939aedeb2885d19e687c8a80013569f26cfd0
Amy Sawyer <amyleighsawyer97@gmail.com>
2024-02-23 02:18:09 +0000

    added a static root to setttings.py file

--- elite_fitness/settings.py
+++ elite_fitness/settings.py
@@ -170,6 +170,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+# Define the directory where collectstatic will collect static files during deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
