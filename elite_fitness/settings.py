c9976814269bf312c8f179c066ac8d638637e38c
SeanGareth505 <seangareth505@gmail.com>
2024-02-24 22:09:34 +0200

    Applied static setting
    Made corrections to the static file path

--- elite_fitness/settings.py
+++ elite_fitness/settings.py
@@ -181,13 +181,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 # Define the directory where collectstatic will collect static files during deployment
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
