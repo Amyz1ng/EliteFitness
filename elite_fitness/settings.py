e0613b89e515d62409114bc9f192df4fea7715e0
Amy Sawyer <amyleighsawyer97@gmail.com>
2024-02-25 13:15:34 +0000

    sending real emails

--- elite_fitness/settings.py
+++ elite_fitness/settings.py
@@ -115,8 +115,6 @@ AUTHENTICATION_BACKENDS = (
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -190,6 +188,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # MEDIA_URL = '/media/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
 
 # if 'USE_AWS' in os.environ:
 #     # Bucket Config
@@ -234,6 +238,18 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', 'pk_test_51Ok6WJELhiwlicG8NTb
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', 'sk_test_51Ok6WJELhiwlicG88i7mLHjJsKiWg3IpubtIoLWPFIGT6p9KvZy0FevcpwcxEmjuKKl8pclg07BzYGuywi5VjkKr00SM7d3GKK')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', 'whsec_ImuaDSppMrq2dkx79CMcFE8iRLoxqRBI')
 DEFAULT_FROM_EMAIL = 'elitefitness@example.com'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'elitefitness@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 # Import the sys module
 import sys
