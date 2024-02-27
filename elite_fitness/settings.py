665177484ef3a5f7030c0216c0f5342dda511d41
SeanGareth505 <seangareth505@gmail.com>
2024-02-27 19:09:00 +0200

    REverted changes

--- elite_fitness/settings.py
+++ elite_fitness/settings.py
@@ -73,7 +73,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'elite_fitness.urls'
