24592dd23191ebb452a0a158d257f49438b96e62
SeanGareth505 <seangareth505@gmail.com>
2024-02-27 18:51:55 +0200

    test email

--- elite_fitness/settings.py
+++ elite_fitness/settings.py
@@ -240,7 +240,7 @@ DEFAULT_FROM_EMAIL = 'elitefitness@example.com'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'elitefitness@example.com'
+    DEFAULT_FROM_EMAIL = 'roagaming1@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
